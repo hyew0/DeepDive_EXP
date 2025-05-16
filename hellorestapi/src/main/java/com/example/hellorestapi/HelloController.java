d0c064cb7a28a1b821d0c901940be1d94b01fc4e
hw <lhy3899@gmail.com>
2025-05-16 20:25:45 +0900

    add HelloController

--- hellorestapi/src/main/java/com/example/hellorestapi/HelloController.java
+++ hellorestapi/src/main/java/com/example/hellorestapi/HelloController.java
@@ -1,4 +1,13 @@
 package com.example.hellorestapi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello-rest-api")
+    public String hello() {
+        return "Hello world";
+    }
 }

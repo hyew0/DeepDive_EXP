628ba770f4a76c72c7e262c7a36b4a4ef8d86ad9
hw <lhy3899@gmail.com>
2025-05-22 13:30:02 +0900

    di,ioc, aop 예제 폴더 추가

---
+++ DiIoCAop/src/test/java/com/example/DiIoCAop/DiIoCAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DiIoCAop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiIoCAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
